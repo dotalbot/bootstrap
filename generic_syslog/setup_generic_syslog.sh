4df316deebf97b3f6ae35300cf2e28bbbb3fa6e2
osvm <osvm@ubuntu.(none)>
2013-05-30 17:35:06 +0200

    Inclusion of syslogng + default config file

--- generic_syslog/setup_generic_syslog.sh
+++ generic_syslog/setup_generic_syslog.sh
@@ -2,7 +2,30 @@
 
 sudo service rsyslog stop
 
-sudo sed -i -e 's|#$ModLoad imudp|$ModLoad imudp|' /etc/rsyslog.conf
-sudo sed -i -e 's|#$UDPServerRun 514|$UDPServerRun 514|' /etc/rsyslog.conf
+#curl https://packages.madhouse-project.org/debian/archive-key.txt | sudo apt-key add -
 
-sudo service rsyslog start
+#cat <<EOF >> sources.add 
+## Repo's for syslog-ng
+#deb       http://packages.madhouse-project.org/debian   wheezy   syslog-ng-3.3
+#deb-src   http://packages.madhouse-project.org/debian   wheezy   syslog-ng-3.3
+#EOF
+
+#sudo bash -c "cat sources.add >> /etc/apt/sources.list"
+#rm -rf sources.add
+
+
+# Syslog ng install without the mad house
+sudo apt-get update
+sudo apt-get install syslog-ng -y
+
+sudo cp -rf syslog-ng.conf /etc/syslog-ng/
+
+
+#sudo apt-get install syslog-ng-core -y
+#sudo apt-get install syslog-ng-mod-sql -y
+#sudo apt-get install syslog-ng-mod-mongodb -y
+#sudo apt-get install syslog-ng-mod-json -y
+#sudo apt-get install syslog-ng -y
+
+
+
